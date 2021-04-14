e3bbcd2ec1b239ba43e6af42a36bc5eedf67c8f8
unknown <gabriel.coelhox@gmail.com>
2021-04-14 18:11:12 -0300

    Corrigindo bug do aviso de erro ficar spamando

--- script.js
+++ script.js
@@ -52,12 +52,16 @@ let Validador = {
             input.parentElement.insertBefore(errorElement, input.ElementSibling);
         },
         clearErrors:() => {
+            let inputs = form.querySelectorAll('input');
+            for(let i=0;i<inputs.length;i++) {
+                inputs[i].style = '';
+            }
+
             let errorElements = document.querySelectorAll('.error');
-            for(let i=0;i<errorElements.length;i++){
+            for(let i=0;i<errorElements.length;i++) {
                 errorElements[i].remove();
             }
         }
-        
 }
 
 let form = document.querySelector('.validador');
